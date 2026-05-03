a74f64fbe58a41aaa86dd7ebc2f3369893abddb5
Ivan <nexacoders2@gmail.com>
2026-05-04 00:41:49 +0530

    Update bot.py

--- Nexa/bot.py
+++ Nexa/bot.py
@@ -1,37 +1,13 @@
-import json
-import os
 from telethon import TelegramClient, events
-from Nexa.config import API_ID, API_HASH, BOT_TOKEN, SESSIONS_FILE
-from Nexa.manager import start_client
+from Nexa.config import API_ID, API_HASH, BOT_TOKEN
 
-bot = TelegramClient("bot", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-
-def save_session(user_id, string):
-    data = {}
-    if os.path.exists(SESSIONS_FILE):
-        with open(SESSIONS_FILE) as f:
-            data = json.load(f)
-
-    data[str(user_id)] = string
-
-    with open(SESSIONS_FILE, "w") as f:
-        json.dump(data, f, indent=2)
+bot = TelegramClient("bot", API_ID, API_HASH)
 
 @bot.on(events.NewMessage(pattern="/start"))
 async def start(event):
-    await event.reply("Send /add <string_session>")
+    await event.reply("Bot working ✅")
 
-@bot.on(events.NewMessage(pattern=r"/add (.+)"))
-async def add_session(event):
-    string = event.pattern_match.group(1)
-    user_id = event.sender_id
-
-    try:
-        await start_client(user_id, string)
-        save_session(user_id, string)
-        await event.reply("Added")
-    except Exception:
-        await event.reply("Invalid session")
-
-def run_bot():
-    bot.run_until_disconnected()
+async def start_bot():
+    await bot.start(bot_token=BOT_TOKEN)
+    print("🤖 Control Bot Started")
+    await bot.run_until_disconnected()
