288e7a7dea36a94e50750fc94f4ce735d08d2ae2
Ivan <nexacoders2@gmail.com>
2026-05-04 01:03:27 +0530

    Update start.py

--- Nexa/plugins/start.py
+++ Nexa/plugins/start.py
@@ -4,4 +4,4 @@ def setup(client):
     @client.on(events.NewMessage(pattern=r"\.start$", outgoing=True))
     async def start(event):
         await event.delete()
-        await client.send_message(event.chat_id, "Nexa active")
+        await client.send_message(event.chat_id, "𝐁𝐎𝐋𝐎 𝐌𝐀𝐋𝐈𝐊 𝐊𝐈𝐒𝐊𝐎 𝐏𝐄𝐋𝐍𝐀 😈")
