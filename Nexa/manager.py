2bd62d712b9ec3fe13a9cb94e7a2920135df67b5
Ivan <nexacoders2@gmail.com>
2026-05-04 00:14:12 +0530

    Create manager.py

---
+++ Nexa/manager.py
@@ -0,0 +1,20 @@
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from Nexa.config import API_ID, API_HASH
+
+clients = {}
+
+async def start_client(user_id, string):
+    if user_id in clients:
+        return clients[user_id]
+
+    client = TelegramClient(StringSession(string), API_ID, API_HASH)
+    await client.start()
+    clients[user_id] = client
+    return client
+
+async def stop_client(user_id):
+    client = clients.get(user_id)
+    if client:
+        await client.disconnect()
+        del clients[user_id]
