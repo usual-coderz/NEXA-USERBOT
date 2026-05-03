919d7c563146c2747f063934a9e052b009788405
Ivan <nexacoders2@gmail.com>
2026-05-04 00:59:08 +0530

    Update start.py

--- Nexa/plugins/start.py
+++ Nexa/plugins/start.py
@@ -1,6 +1,7 @@
 def setup(client):
     from telethon import events
 
-    @client.on(events.NewMessage(pattern=r"\.start"))
+    @client.on(events.NewMessage(pattern=r"\.start$", outgoing=True))
     async def start(event):
-        await event.reply("Nexa active")
+        await event.delete()
+        await client.send_message(event.chat_id, "Nexa active")
