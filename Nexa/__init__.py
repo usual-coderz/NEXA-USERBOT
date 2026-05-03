66104d6d9a4d622aa34f4ec4c9a181e780768f48
Ivan <nexacoders2@gmail.com>
2026-05-04 00:15:37 +0530

    Create __init__.py

---
+++ Nexa/__init__.py
@@ -0,0 +1,12 @@
+from .config import API_ID, API_HASH, BOT_TOKEN, SESSIONS_FILE
+from .manager import clients, start_client, stop_client
+
+__all__ = [
+    "API_ID",
+    "API_HASH",
+    "BOT_TOKEN",
+    "SESSIONS_FILE",
+    "clients",
+    "start_client",
+    "stop_client",
+]
