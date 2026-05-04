fc635e13f7e3444109748a3549bbc30f1846390a
Ivan <nexacoders2@gmail.com>
2026-05-04 11:17:59 +0530

    Update ping.py

--- Nexa/plugins/ping.py
+++ Nexa/plugins/ping.py
@@ -5,27 +5,23 @@ def setup(client):
 
     async def allowed(event):
         me = await client.get_me()
-        if event.sender_id == me.id:
-            return True
-        if await is_fullsudo(me.id, event.sender_id):
-            return True
-        if await is_sudo(me.id, event.sender_id):
-            return True
-        return False
+        return (
+            event.sender_id == me.id
+            or await is_fullsudo(me.id, event.sender_id)
+            or await is_sudo(me.id, event.sender_id)
+        )
 
     @client.on(events.NewMessage(pattern=r"\.ping$"))
     async def ping(event):
         if not await allowed(event):
             return
 
-        start = time.time()
+        start = time.perf_counter()
 
         if event.out:
             await event.delete()
 
         msg = await client.send_message(event.chat_id, "Pinging...")
 
-        end = time.time()
-        ms = round((end - start) * 1000)
-
+        ms = int((time.perf_counter() - start) * 1000)
         await msg.edit(f"𝐏𝐎𝐍𝐆 ⚡ {ms} 𝐦𝐬")
