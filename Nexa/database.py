f820acfb4b515355d8cc14862779b8af5b9acbb1
Ivan <nexacoders2@gmail.com>
2026-05-04 13:55:56 +0530

    Update database.py

--- Nexa/database.py
+++ Nexa/database.py
@@ -1,4 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from bson import ObjectId
 from Nexa.config import MONGO_URI, DB_NAME, COLLECTION
 
 mongo = AsyncIOMotorClient(MONGO_URI)
@@ -6,20 +7,28 @@ db = mongo[DB_NAME]
 col = db[COLLECTION]
 
 
-async def add_session(user_id, string):
-    await col.update_one(
-        {"user_id": user_id},
-        {"$set": {"string": string}},
-        upsert=True
-    )
+async def add_session(user_id, string, name):
+    result = await col.insert_one({
+        "user_id": user_id,
+        "string": string,
+        "name": name
+    })
+    return result
+
+
+async def get_sessions(user_id):
+    data = []
+    async for doc in col.find({"user_id": user_id}):
+        data.append(doc)
+    return data
 
 
 async def get_all_sessions():
     data = []
     async for doc in col.find():
-        data.append((doc["user_id"], doc["string"]))
+        data.append(doc)
     return data
 
 
-async def delete_session(user_id):
-    await col.delete_one({"user_id": user_id})
+async def delete_session(session_id):
+    await col.delete_one({"_id": ObjectId(session_id)})
